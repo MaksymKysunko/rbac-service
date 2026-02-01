130bb3116a98edf769487abb50c8410269aabf2b
Maksym Kysunko <mkisunko@gmail.com>
2026-02-01 19:05:52 +0200

    great fix of everything

--- app/auth.py
+++ app/auth.py
@@ -1,4 +1,4 @@
-from app.core.config import JWKS_URL, ALG, BACKEND_AUDIENCE
+from app.core.config import JWKS_URL, ALG, BACKEND_AUDIENCE, IDP_ISS
 from club_shared.auth.settings import AuthSettings
 from club_shared.auth.deps import build_auth_deps
 
@@ -6,6 +6,7 @@ settings = AuthSettings(
     jwks_url=JWKS_URL,
     algorithm=ALG,
     audience=BACKEND_AUDIENCE,
+    issuer=IDP_ISS
 )
 
 get_claims, get_principal, require_role = build_auth_deps(settings)
--- app/core/config.py
+++ app/core/config.py
@@ -3,5 +3,5 @@ import os
 
 ALG = "RS256"
 JWKS_URL = os.getenv("JWKS_URL", "http://idp:8000/.well-known/jwks.json")
-IDP_ISS = os.getenv("IDP_ISS", "club-idp")
+IDP_ISS = os.getenv("IDP_ISS", "http://idp:8000")
 BACKEND_AUDIENCE = os.getenv("BACKEND_AUDIENCE", "club-api")
